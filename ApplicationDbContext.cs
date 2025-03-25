38dff0e458f9fd55e09ff29026ca30decd3dfda5
abela69 <107638481+abela69@users.noreply.github.com>
2025-03-25 19:06:45 +0400

    Add files via upload

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Project.Model;
+
+namespace Project
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<CartItem> CartItems { get; set; }
+
+    }
+}
